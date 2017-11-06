b41a9420b9b56d36c2c55ab044e5cb953d290ef3
Jeremy <jeremyd23@outlook.com>
2017-11-06 13:49:58 -0800

    Added comments to tests.

--- src/test/java/GameboardTest.java
+++ src/test/java/GameboardTest.java
@@ -1,12 +1,16 @@
 import junit.framework.TestCase;
 
+/**
+ * Tests the different methods of the GameBoard class.
+ */
 public class GameboardTest extends TestCase
 {
-    public GameboardTest(String name) {
-        super(name);
-    }
-
-    public void testAddCell(){
+    /**
+     * Asserts that one Cell exists when GameBoard is instantiated and getCellNumber() returns the correct number of Cells.
+     * Asserts that getCellNumber() returns 2 when adding an additional Cell.
+     */
+    public void testAddCell()
+    {
         GameBoard gameboard = new GameBoard();
         assertEquals(1, gameboard.getCellNumber());
         Cell cell = new Cell();
@@ -14,6 +18,9 @@ public class GameboardTest extends TestCase
         assertEquals(2, gameboard.getCellNumber());
     }
 
+    /**
+     * Asserts first Cell in GameBoard is of type GoCell.
+     */
     public void testFirstCell()
     {
         GameBoard gameboard = new GameBoard();
@@ -21,6 +28,10 @@ public class GameboardTest extends TestCase
         assertSame(GoCell.class, firstCell.getClass());
     }
 
+    /**
+     * Asserts that when a Cell is added to specifc index of GameBoard, getCellIndex(Cell) returns the correct index.
+     * Asserts that getCellIndex returns -1 if a given Cell is not in GameBoard.
+     */
     public void testGetCellIndex()
     {
         GameBoard gameBoard = new SimpleGameBoard();
